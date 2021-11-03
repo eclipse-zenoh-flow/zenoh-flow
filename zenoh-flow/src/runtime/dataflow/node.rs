54e799d35075c9f4ccb59f7f159bbb1add0c6d6a
Julien Loudet <julien.loudet@adlinktech.com>
2021-11-03 17:28:00 +0100

    cleanup: remove `DataFlowNode` and `DataFlowNodeKind`

--- zenoh-flow/src/runtime/dataflow/node.rs
+++ zenoh-flow/src/runtime/dataflow/node.rs
@@ -15,15 +15,13 @@
 use std::collections::HashMap;
 use std::convert::TryFrom;
 
-use crate::model::connector::{ZFConnectorKind, ZFConnectorRecord};
 use crate::model::link::PortDescriptor;
 use crate::model::node::{OperatorRecord, SinkRecord, SourceRecord};
 use crate::model::period::PeriodDescriptor;
 use crate::runtime::dataflow::loader::{load_operator, load_sink, load_source};
-use crate::{NodeId, Operator, PortId, RuntimeId, Sink, Source, State, ZFError, ZFResult};
+use crate::{NodeId, Operator, PortId, Sink, Source, State, ZFError};
 use async_std::sync::{Arc, RwLock};
 use libloading::Library;
-use serde::{Deserialize, Serialize};
 
 pub struct SourceLoaded {
     pub(crate) id: NodeId,
@@ -131,142 +129,5 @@ impl TryFrom<SinkRecord> for SinkLoaded {
             sink,
             library: Some(Arc::new(library)),
         })
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum DataFlowNodeKind {
-    Operator,
-    Source,
-    Sink,
-    Connector,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum DataFlowNode {
-    Operator(OperatorRecord),
-    Source(SourceRecord),
-    Sink(SinkRecord),
-    Connector(ZFConnectorRecord),
-}
-
-impl std::fmt::Display for DataFlowNode {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            DataFlowNode::Operator(inner) => write!(f, "{}", inner),
-            DataFlowNode::Source(inner) => write!(f, "{}", inner),
-            DataFlowNode::Sink(inner) => write!(f, "{}", inner),
-            DataFlowNode::Connector(inner) => write!(f, "{}", inner),
-        }
-    }
-}
-
-impl DataFlowNode {
-    pub fn has_input(&self, id: String) -> bool {
-        match self {
-            DataFlowNode::Operator(op) => match op.inputs.iter().find(|&lid| *lid.port_id == id) {
-                Some(_lid) => true,
-                None => false,
-            },
-            DataFlowNode::Source(_) => false,
-            DataFlowNode::Sink(sink) => sink.input.port_id == id,
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => false,
-                ZFConnectorKind::Sender => zc.link_id.port_id == id,
-            },
-        }
-    }
-
-    pub fn get_input_type(&self, id: String) -> ZFResult<&str> {
-        match self {
-            DataFlowNode::Operator(op) => match op.inputs.iter().find(|&lid| *lid.port_id == id) {
-                Some(lid) => Ok(&lid.port_type),
-                None => Err(ZFError::PortNotFound((self.get_id(), id))),
-            },
-            DataFlowNode::Source(_) => Err(ZFError::PortNotFound((self.get_id(), id))),
-            DataFlowNode::Sink(sink) => {
-                if sink.input.port_id == id {
-                    Ok(&sink.input.port_type)
-                } else {
-                    Err(ZFError::PortNotFound((self.get_id(), id)))
-                }
-            }
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => Err(ZFError::PortNotFound((self.get_id(), id))),
-                ZFConnectorKind::Sender => {
-                    if zc.link_id.port_id == id {
-                        Ok(&zc.link_id.port_type)
-                    } else {
-                        Err(ZFError::PortNotFound((self.get_id(), id)))
-                    }
-                }
-            },
-        }
-    }
-
-    pub fn has_output(&self, id: String) -> bool {
-        match self {
-            DataFlowNode::Operator(op) => match op.outputs.iter().find(|&lid| *lid.port_id == id) {
-                Some(_lid) => true,
-                None => false,
-            },
-            DataFlowNode::Sink(_) => false,
-            DataFlowNode::Source(source) => source.output.port_id == id,
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => zc.link_id.port_id == id,
-                ZFConnectorKind::Sender => false,
-            },
-        }
-    }
-
-    pub fn get_output_type(&self, id: String) -> ZFResult<&str> {
-        match self {
-            DataFlowNode::Operator(op) => match op.outputs.iter().find(|&lid| *lid.port_id == id) {
-                Some(lid) => Ok(&lid.port_type),
-                None => Err(ZFError::PortNotFound((self.get_id(), id))),
-            },
-            DataFlowNode::Sink(_) => Err(ZFError::PortNotFound((self.get_id(), id))),
-            DataFlowNode::Source(source) => {
-                if source.output.port_id == id {
-                    Ok(&source.output.port_type)
-                } else {
-                    Err(ZFError::PortNotFound((self.get_id(), id)))
-                }
-            }
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => {
-                    if zc.link_id.port_id == id {
-                        Ok(&zc.link_id.port_type)
-                    } else {
-                        Err(ZFError::PortNotFound((self.get_id(), id)))
-                    }
-                }
-                ZFConnectorKind::Sender => Err(ZFError::PortNotFound((self.get_id(), id))),
-            },
-        }
-    }
-
-    pub fn get_id(&self) -> NodeId {
-        match self {
-            DataFlowNode::Operator(op) => op.id.clone(),
-            DataFlowNode::Sink(s) => s.id.clone(),
-            DataFlowNode::Source(s) => s.id.clone(),
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => zc.id.clone(),
-                ZFConnectorKind::Sender => zc.id.clone(),
-            },
-        }
-    }
-
-    pub fn get_runtime(&self) -> RuntimeId {
-        match self {
-            DataFlowNode::Operator(op) => op.runtime.clone(),
-            DataFlowNode::Sink(s) => s.runtime.clone(),
-            DataFlowNode::Source(s) => s.runtime.clone(),
-            DataFlowNode::Connector(zc) => match zc.kind {
-                ZFConnectorKind::Receiver => zc.runtime.clone(),
-                ZFConnectorKind::Sender => zc.runtime.clone(),
-            },
-        }
     }
 }
