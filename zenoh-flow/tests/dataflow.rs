658f1429dfcefd16121fc4e20ff7c9fe12367b8c
gabrik <gabriele.baldoni@gmail.com>
2022-08-10 16:07:42 +0200

    updated dataflow.rs tests
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- zenoh-flow/tests/dataflow.rs
+++ zenoh-flow/tests/dataflow.rs
@@ -12,295 +12,281 @@
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
 
-// use async_std::sync::Arc;
-// use async_trait::async_trait;
-// use flume::{bounded, Receiver};
-// use std::collections::HashMap;
-// use std::convert::TryInto;
-// use std::sync::atomic::{AtomicUsize, Ordering};
-// use zenoh_flow::model::link::PortDescriptor;
-// use zenoh_flow::model::{InputDescriptor, OutputDescriptor};
-// use zenoh_flow::runtime::dataflow::instance::DataflowInstance;
-// use zenoh_flow::runtime::dataflow::loader::{Loader, LoaderConfig};
-// use zenoh_flow::runtime::RuntimeContext;
-// use zenoh_flow::zenoh_flow_derive::ZFData;
-// use zenoh_flow::{
-//     default_input_rule, default_output_rule, zf_empty_state, Configuration, Context, Data,
-//     Deserializable, Node, NodeOutput, Operator, PortId, Sink, Source, State, ZFData, ZFError,
-//     ZFResult,
-// };
+#![feature(async_closure)]
 
-// // Data Type
+use async_std::sync::Arc;
+use async_trait::async_trait;
+use flume::{bounded, Receiver};
+use std::convert::TryInto;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use zenoh_flow::model::link::PortDescriptor;
+use zenoh_flow::model::{InputDescriptor, OutputDescriptor};
+use zenoh_flow::runtime::dataflow::instance::DataflowInstance;
+use zenoh_flow::runtime::dataflow::loader::{Loader, LoaderConfig};
+use zenoh_flow::runtime::RuntimeContext;
+use zenoh_flow::zenoh_flow_derive::ZFData;
+use zenoh_flow::{
+    AsyncIteration, Configuration, Context, Data, Deserializable, Inputs, Message, Node, Operator,
+    Outputs, Sink, Source, Streams, ZFData, ZFError, ZFResult,
+};
 
-// #[derive(Debug, Clone, ZFData)]
-// pub struct ZFUsize(pub usize);
+// Data Type
 
-// impl ZFData for ZFUsize {
-//     fn try_serialize(&self) -> ZFResult<Vec<u8>> {
-//         Ok(self.0.to_ne_bytes().to_vec())
-//     }
-// }
+#[derive(Debug, Clone, ZFData)]
+pub struct ZFUsize(pub usize);
 
-// impl Deserializable for ZFUsize {
-//     fn try_deserialize(bytes: &[u8]) -> ZFResult<Self>
-//     where
-//         Self: Sized,
-//     {
-//         let value =
-//             usize::from_ne_bytes(bytes.try_into().map_err(|_| ZFError::DeseralizationError)?);
-//         Ok(ZFUsize(value))
-//     }
-// }
+impl ZFData for ZFUsize {
+    fn try_serialize(&self) -> ZFResult<Vec<u8>> {
+        Ok(self.0.to_ne_bytes().to_vec())
+    }
+}
 
-// static SOURCE: &str = "Counter";
-// static DESTINATION: &str = "Counter";
+impl Deserializable for ZFUsize {
+    fn try_deserialize(bytes: &[u8]) -> ZFResult<Self>
+    where
+        Self: Sized,
+    {
+        let value =
+            usize::from_ne_bytes(bytes.try_into().map_err(|_| ZFError::DeseralizationError)?);
+        Ok(ZFUsize(value))
+    }
+}
 
-// static COUNTER: AtomicUsize = AtomicUsize::new(0);
+static SOURCE: &str = "Counter";
+static DESTINATION: &str = "Counter";
 
-// // SOURCE
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-// struct CountSource {
-//     rx: Receiver<()>,
-// }
+// SOURCE
 
-// unsafe impl Send for CountSource {}
-// unsafe impl Sync for CountSource {}
+struct CountSource {
+    rx: Receiver<()>,
+}
 
-// impl CountSource {
-//     pub fn new(rx: Receiver<()>) -> Self {
-//         CountSource { rx }
-//     }
-// }
+unsafe impl Send for CountSource {}
+unsafe impl Sync for CountSource {}
 
-// #[async_trait]
-// impl Source for CountSource {
-//     async fn run(&self, _context: &mut Context, _state: &mut State) -> zenoh_flow::ZFResult<Data> {
-//         let _ = self.rx.recv_async().await;
-//         COUNTER.fetch_add(1, Ordering::AcqRel);
-//         let d = ZFUsize(COUNTER.load(Ordering::Relaxed));
-//         Ok(Data::from::<ZFUsize>(d))
-//     }
-// }
+impl CountSource {
+    pub fn new(rx: Receiver<()>) -> Self {
+        CountSource { rx }
+    }
+}
 
-// impl Node for CountSource {
-//     fn initialize(&self, _configuration: &Option<Configuration>) -> ZFResult<State> {
-//         zf_empty_state!()
-//     }
+#[async_trait]
+impl Source for CountSource {
+    async fn setup(
+        &self,
+        _ctx: &mut Context,
+        configuration: &Option<Configuration>,
+        mut outputs: Outputs,
+    ) -> ZFResult<Option<Arc<dyn AsyncIteration>>> {
+        let output = outputs.take(SOURCE).unwrap();
+        let c_trigger_rx = self.rx.clone();
 
-//     fn finalize(&self, _state: &mut State) -> ZFResult<()> {
-//         Ok(())
-//     }
-// }
+        Ok(Some(Arc::new(async move || {
+            COUNTER.fetch_add(1, Ordering::AcqRel);
+            let d = ZFUsize(COUNTER.load(Ordering::Relaxed));
+            let data = Data::from::<ZFUsize>(d);
+            output.send_async(data, None).await
+        })))
+    }
+}
 
-// // SINK
+#[async_trait]
+impl Node for CountSource {
+    async fn finalize(&self) -> ZFResult<()> {
+        Ok(())
+    }
+}
 
-// struct ExampleGenericSink;
+// SINK
 
-// #[async_trait]
-// impl Sink for ExampleGenericSink {
-//     async fn run(
-//         &self,
-//         _context: &mut Context,
-//         _state: &mut State,
-//         mut input: zenoh_flow::runtime::message::DataMessage,
-//     ) -> zenoh_flow::ZFResult<()> {
-//         let data = input.get_inner_data().try_get::<ZFUsize>()?;
+struct ExampleGenericSink;
 
-//         assert_eq!(data.0, COUNTER.load(Ordering::Relaxed));
+#[async_trait]
+impl Sink for ExampleGenericSink {
+    async fn setup(
+        &self,
+        _ctx: &mut Context,
+        configuration: &Option<Configuration>,
+        mut inputs: Inputs,
+    ) -> ZFResult<Option<Arc<dyn AsyncIteration>>> {
+        let input = inputs.take(SOURCE).unwrap();
 
-//         println!("Example Generic Sink Received: {:?}", input);
-//         Ok(())
-//     }
-// }
+        Ok(Some(Arc::new(async move || {
+            if let Ok(Message::Data(mut msg)) = input.recv_async().await {
+                let data = msg.get_inner_data().try_get::<ZFUsize>()?;
+                assert_eq!(data.0, COUNTER.load(Ordering::Relaxed));
+                println!("Example Generic Sink Received: {:?}", input);
+            }
+            Ok(())
+        })))
+    }
+}
 
-// impl Node for ExampleGenericSink {
-//     fn initialize(&self, _configuration: &Option<Configuration>) -> ZFResult<State> {
-//         zf_empty_state!()
-//     }
+#[async_trait]
+impl Node for ExampleGenericSink {
+    async fn finalize(&self) -> ZFResult<()> {
+        Ok(())
+    }
+}
 
-//     fn finalize(&self, _state: &mut State) -> ZFResult<()> {
-//         Ok(())
-//     }
-// }
+// OPERATOR
 
-// // OPERATOR
+#[derive(Debug)]
+struct NoOp;
 
-// #[derive(Debug)]
-// struct NoOp;
+#[async_trait]
+impl Operator for NoOp {
+    async fn setup(
+        &self,
+        _ctx: &mut Context,
+        _configuration: &Option<Configuration>,
+        mut inputs: Inputs,
+        mut outputs: Outputs,
+    ) -> ZFResult<Option<Arc<dyn AsyncIteration>>> {
+        let input = inputs.take(SOURCE).unwrap();
+        let output = outputs.take(DESTINATION).unwrap();
 
-// impl Operator for NoOp {
-//     fn input_rule(
-//         &self,
-//         _context: &mut zenoh_flow::Context,
-//         state: &mut State,
-//         tokens: &mut HashMap<PortId, zenoh_flow::InputToken>,
-//     ) -> zenoh_flow::ZFResult<bool> {
-//         default_input_rule(state, tokens)
-//     }
+        Ok(Some(Arc::new(async move || {
+            if let Ok(Message::Data(mut msg)) = input.recv_async().await {
+                let data = msg.get_inner_data().try_get::<ZFUsize>()?;
+                assert_eq!(data.0, COUNTER.load(Ordering::Relaxed));
+            }
+            Ok(())
+        })))
+    }
+}
 
-//     fn run(
-//         &self,
-//         _context: &mut zenoh_flow::Context,
-//         _state: &mut State,
-//         inputs: &mut HashMap<PortId, zenoh_flow::runtime::message::DataMessage>,
-//     ) -> zenoh_flow::ZFResult<HashMap<zenoh_flow::PortId, Data>> {
-//         let mut results: HashMap<PortId, Data> = HashMap::new();
-//         let source: PortId = SOURCE.into();
+#[async_trait]
+impl Node for NoOp {
+    async fn finalize(&self) -> ZFResult<()> {
+        Ok(())
+    }
+}
 
-//         let data = inputs
-//             .get_mut(&source)
-//             .ok_or_else(|| ZFError::InvalidData("No data".to_string()))?
-//             .get_inner_data()
-//             .try_get::<ZFUsize>()?;
+// Run dataflow in single runtime
+async fn single_runtime() {
+    env_logger::init();
 
-//         assert_eq!(data.0, COUNTER.load(Ordering::Relaxed));
+    let (tx, rx) = bounded::<()>(1); // Channel used to trigger source
 
-//         results.insert(DESTINATION.into(), Data::from::<ZFUsize>(data.clone()));
-//         Ok(results)
-//     }
+    let session = Arc::new(zenoh::open(zenoh::config::Config::default()).await.unwrap());
+    let hlc = async_std::sync::Arc::new(uhlc::HLC::default());
+    let rt_uuid = uuid::Uuid::new_v4();
+    let ctx = RuntimeContext {
+        session,
+        hlc: hlc.clone(),
+        loader: Arc::new(Loader::new(LoaderConfig::new())),
+        runtime_name: format!("test-runtime-{}", rt_uuid).into(),
+        runtime_uuid: rt_uuid,
+    };
 
-//     fn output_rule(
-//         &self,
-//         _context: &mut zenoh_flow::Context,
-//         state: &mut State,
-//         outputs: HashMap<PortId, Data>,
-//     ) -> zenoh_flow::ZFResult<HashMap<zenoh_flow::PortId, NodeOutput>> {
-//         default_output_rule(state, outputs)
-//     }
-// }
+    let mut dataflow =
+        zenoh_flow::runtime::dataflow::Dataflow::new(ctx.clone(), "test".into(), None);
 
-// impl Node for NoOp {
-//     fn initialize(&self, _configuration: &Option<Configuration>) -> ZFResult<State> {
-//         zf_empty_state!()
-//     }
+    let source = Arc::new(CountSource::new(rx));
+    let sink = Arc::new(ExampleGenericSink {});
+    let operator = Arc::new(NoOp {});
 
-//     fn finalize(&self, _state: &mut State) -> ZFResult<()> {
-//         Ok(())
-//     }
-// }
+    dataflow
+        .try_add_static_source(
+            "counter-source".into(),
+            None,
+            PortDescriptor {
+                port_id: SOURCE.into(),
+                port_type: "int".into(),
+            },
+            source,
+        )
+        .unwrap();
 
-// // Run dataflow in single runtime
-// async fn single_runtime() {
-//     env_logger::init();
+    dataflow
+        .try_add_static_sink(
+            "generic-sink".into(),
+            None,
+            PortDescriptor {
+                port_id: SOURCE.into(),
+                port_type: "int".into(),
+            },
+            sink,
+        )
+        .unwrap();
 
-//     let (tx, rx) = bounded::<()>(1); // Channel used to trigger source
+    dataflow
+        .try_add_static_operator(
+            "noop".into(),
+            None,
+            vec![PortDescriptor {
+                port_id: SOURCE.into(),
+                port_type: "int".into(),
+            }],
+            vec![PortDescriptor {
+                port_id: DESTINATION.into(),
+                port_type: "int".into(),
+            }],
+            operator,
+        )
+        .unwrap();
 
-//     let session = Arc::new(zenoh::open(zenoh::config::Config::default()).await.unwrap());
-//     let hlc = async_std::sync::Arc::new(uhlc::HLC::default());
-//     let rt_uuid = uuid::Uuid::new_v4();
-//     let ctx = RuntimeContext {
-//         session,
-//         hlc,
-//         loader: Arc::new(Loader::new(LoaderConfig::new())),
-//         runtime_name: format!("test-runtime-{}", rt_uuid).into(),
-//         runtime_uuid: rt_uuid,
-//     };
+    dataflow
+        .try_add_link(
+            OutputDescriptor {
+                node: "counter-source".into(),
+                output: SOURCE.into(),
+            },
+            InputDescriptor {
+                node: "noop".into(),
+                input: SOURCE.into(),
+            },
+            None,
+            None,
+            None,
+        )
+        .unwrap();
 
-//     let mut dataflow =
-//         zenoh_flow::runtime::dataflow::Dataflow::new(ctx.clone(), "test".into(), None);
+    dataflow
+        .try_add_link(
+            OutputDescriptor {
+                node: "noop".into(),
+                output: DESTINATION.into(),
+            },
+            InputDescriptor {
+                node: "generic-sink".into(),
+                input: SOURCE.into(),
+            },
+            None,
+            None,
+            None,
+        )
+        .unwrap();
 
-//     let source = Arc::new(CountSource::new(rx));
-//     let sink = Arc::new(ExampleGenericSink {});
-//     let operator = Arc::new(NoOp {});
+    let mut instance = DataflowInstance::try_instantiate(dataflow, hlc.clone()).unwrap();
 
-//     dataflow
-//         .try_add_static_source(
-//             "counter-source".into(),
-//             PortDescriptor {
-//                 port_id: SOURCE.into(),
-//                 port_type: "int".into(),
-//             },
-//             source.initialize(&None).unwrap(),
-//             source,
-//         )
-//         .unwrap();
+    let ids = instance.get_nodes();
+    for id in &ids {
+        instance.start_node(id).await.unwrap();
+    }
+    tx.send_async(()).await.unwrap();
 
-//     dataflow
-//         .try_add_static_sink(
-//             "generic-sink".into(),
-//             PortDescriptor {
-//                 port_id: SOURCE.into(),
-//                 port_type: "int".into(),
-//             },
-//             sink.initialize(&None).unwrap(),
-//             sink,
-//         )
-//         .unwrap();
+    zenoh_flow::async_std::task::sleep(std::time::Duration::from_secs(1)).await;
 
-//     dataflow
-//         .try_add_static_operator(
-//             "noop".into(),
-//             vec![PortDescriptor {
-//                 port_id: SOURCE.into(),
-//                 port_type: "int".into(),
-//             }],
-//             vec![PortDescriptor {
-//                 port_id: DESTINATION.into(),
-//                 port_type: "int".into(),
-//             }],
-//             operator.initialize(&None).unwrap(),
-//             operator,
-//         )
-//         .unwrap();
+    for id in &instance.get_sources() {
+        instance.stop_node(id).await.unwrap()
+    }
 
-//     dataflow
-//         .try_add_link(
-//             OutputDescriptor {
-//                 node: "counter-source".into(),
-//                 output: SOURCE.into(),
-//             },
-//             InputDescriptor {
-//                 node: "noop".into(),
-//                 input: SOURCE.into(),
-//             },
-//             None,
-//             None,
-//             None,
-//         )
-//         .unwrap();
+    for id in &instance.get_operators() {
+        instance.stop_node(id).await.unwrap()
+    }
 
-//     dataflow
-//         .try_add_link(
-//             OutputDescriptor {
-//                 node: "noop".into(),
-//                 output: DESTINATION.into(),
-//             },
-//             InputDescriptor {
-//                 node: "generic-sink".into(),
-//                 input: SOURCE.into(),
-//             },
-//             None,
-//             None,
-//             None,
-//         )
-//         .unwrap();
+    for id in &instance.get_sinks() {
+        instance.stop_node(id).await.unwrap()
+    }
+}
 
-//     let mut instance = DataflowInstance::try_instantiate(dataflow).unwrap();
+#[test]
+fn run_single_runtime() {
+    let h1 = async_std::task::spawn(async move { single_runtime().await });
 
-//     let ids = instance.get_nodes();
-//     for id in &ids {
-//         instance.start_node(id).await.unwrap();
-//     }
-//     tx.send_async(()).await.unwrap();
-
-//     zenoh_flow::async_std::task::sleep(std::time::Duration::from_secs(1)).await;
-
-//     for id in &instance.get_sources() {
-//         instance.stop_node(id).await.unwrap()
-//     }
-
-//     for id in &instance.get_operators() {
-//         instance.stop_node(id).await.unwrap()
-//     }
-
-//     for id in &instance.get_sinks() {
-//         instance.stop_node(id).await.unwrap()
-//     }
-// }
-
-// #[test]
-// fn run_single_runtime() {
-//     let h1 = async_std::task::spawn(async move { single_runtime().await });
-
-//     async_std::task::block_on(async move { h1.await })
-// }
+    async_std::task::block_on(async move { h1.await })
+}
