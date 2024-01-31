e254290e9c28c911bef88e3dcf982e8b429e2490
Julien Loudet <julien.loudet@zettascale.tech>
2024-01-31 16:50:26 +0100

    feat(standalone-daemon): (wip) standalone Zenoh-Flow daemon
    The standalone daemon is an executable version of the Zenoh-Flow daemon.
    
    By default it connects to the local Zenoh network as a peer. It allows setting
    the identifier of the underlying Zenoh-Flow runtime through a command line
    argument.
    
    (WIP) Although it also accepts a configuration through command line parameters,
    this parameter is for now ignored.
    
    Signed-off-by: Julien Loudet <julien.loudet@zettascale.tech>

---
+++ zenoh-flow-standalone-daemon/src/main.rs
@@ -0,0 +1,74 @@
+//
+// Copyright (c) 2021 - 2024 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+use std::{path::PathBuf, sync::Arc};
+
+use async_std::stream::StreamExt;
+use clap::Parser;
+use signal_hook::consts::{SIGINT, SIGQUIT, SIGTERM};
+use signal_hook_async_std::Signals;
+use uhlc::HLC;
+use zenoh::prelude::r#async::*;
+use zenoh_flow_daemon_2::DaemonBuilder;
+
+#[derive(Parser)]
+struct Cli {
+    name: Arc<str>,
+    #[clap(short, long)]
+    configuration: Option<PathBuf>,
+    #[clap(short, long)]
+    runtime_id: Option<ZenohId>,
+}
+
+#[async_std::main]
+async fn main() {
+    let _ = tracing_subscriber::fmt::try_init();
+
+    let cli = Cli::parse();
+
+    let zenoh_session = zenoh::open(zenoh::config::peer())
+        .res()
+        .await
+        .expect("Failed to open Zenoh session in PEER mode.")
+        .into_arc();
+
+    let hlc = Arc::new(HLC::default());
+
+    let mut builder = DaemonBuilder::new(zenoh_session, hlc, cli.name);
+    if let Some(runtime_id) = cli.runtime_id {
+        builder = builder.runtime_id(runtime_id.into());
+    }
+
+    let daemon = builder.start().await;
+
+    async_std::task::spawn(async move {
+        let mut signals = Signals::new([SIGTERM, SIGINT, SIGQUIT])
+            .expect("Failed to create SignalsInfo for: [SIGTERM, SIGINT, SIGQUIT]");
+
+        while let Some(signal) = signals.next().await {
+            match signal {
+                SIGTERM | SIGINT | SIGQUIT => {
+                    tracing::info!("Received termination signal, shutting down.");
+                    daemon.stop().await;
+                    break;
+                }
+
+                _ => {
+                    unreachable!()
+                }
+            }
+        }
+    })
+    .await;
+}
